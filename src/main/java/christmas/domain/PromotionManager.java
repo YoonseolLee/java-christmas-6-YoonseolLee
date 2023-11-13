063aa7c8b5ce4ad27237517769666a0c146a2e85
Yoonseol Lee <thisyoon97@gmail.com>
2023-11-13 13:37:43 +0900

    style: 주석 삭제 불필요하므로 삭제하였음

--- src/main/java/christmas/domain/PromotionManager.java
+++ src/main/java/christmas/domain/PromotionManager.java
@@ -3,23 +3,15 @@ package christmas.domain;
 import christmas.view.OutputView;
 import java.util.Optional;
 import java.util.function.Supplier;
+import java.util.stream.Stream;
 
 public class PromotionManager {
-    //    public <T> T getValidInput(Supplier<T> inputSupplier) {
-//        return Stream.generate(() -> tryGet(inputSupplier))
-//                .filter(Optional::isPresent)
-//                .map(Optional::get)
-//                .findFirst()
-//                .orElseThrow();
-//    }
     public <T> T getValidInput(Supplier<T> inputSupplier) {
-        while (true) {
-            try {
-                return inputSupplier.get();
-            } catch (IllegalArgumentException e) {
-                OutputView.printException(e);
-            }
-        }
+        return Stream.generate(() -> tryGet(inputSupplier))
+                .filter(Optional::isPresent)
+                .map(Optional::get)
+                .findFirst()
+                .orElseThrow();
     }
 
     private <T> Optional<T> tryGet(Supplier<T> inputSupplier) {
