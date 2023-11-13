6c8f616c9d8d4b6593a2ffb91d3e159124e4fb1b
Yoonseol Lee <thisyoon97@gmail.com>
2023-11-13 20:07:49 +0900

    refactor: PromotionManager 삭제 더이상 필요하지 않아서 삭제하였음

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -1,11 +1,10 @@
 package christmas;
 
 import christmas.controller.MainController;
-import christmas.domain.PromotionManager;
 
 public class Application {
     public static void main(String[] args) {
-        MainController controller = new MainController(new PromotionManager());
+        MainController controller = new MainController();
         controller.start();
     }
 }
--- src/main/java/christmas/controller/MainController.java
+++ src/main/java/christmas/controller/MainController.java
@@ -1,18 +1,11 @@
 package christmas.controller;
 
 import christmas.domain.Order;
-import christmas.domain.PromotionManager;
 import christmas.domain.VisitingDate;
 import christmas.view.InputView;
 import christmas.view.OutputView;
 
 public class MainController {
-    private final PromotionManager manager;
-
-    public MainController(final PromotionManager manager) {
-        this.manager = manager;
-    }
-
     public void start() {
         OutputView.printGreetings();
         VisitingDate visitingDate = InputView.getVisitingDate();
