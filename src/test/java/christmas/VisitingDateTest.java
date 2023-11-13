f8a496ad806646d939108346c4790222a41490d3
Yoonseol Lee <thisyoon97@gmail.com>
2023-11-13 19:56:39 +0900

    test:VisitingDate에 대한 테스트코드

--- src/test/java/christmas/VisitingDateTest.java
+++ src/test/java/christmas/VisitingDateTest.java
@@ -1,24 +1,27 @@
 package christmas;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-import christmas.domain.VisitingDate;
-import java.time.LocalDate;
+import christmas.domain.validation.VisitingDateValidation;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class VisitingDateTest {
-    @DisplayName("VisitingDate 유효성 검사")
-    @Test
-    void 일을_입력하면_23년_12월_23일을_반환한다() {
-        // Given
-        int day = 2;
+    @ParameterizedTest
+    @ValueSource(strings = {"00", "  ", "!", "32", "-1", ""})
+    @DisplayName("isValidDate 테스트")
+    void 입력한_날짜가_유효하지_않으면_예외_발생(String invalidDate) {
+        // When, Then
+        Assertions.assertThrows(IllegalArgumentException.class,
+                () -> VisitingDateValidation.isValidDate(invalidDate));
+    }
 
-        // When
-        VisitingDate visitingDate = new VisitingDate(day);
-
-        // Then
-        LocalDate expectedDate = LocalDate.of(2023, 12, day);
-        assertEquals(expectedDate, visitingDate.getVisitingDate());
+    @ParameterizedTest
+    @ValueSource(ints = {0, -1, 33, 100})
+    @DisplayName("isValidDate 테스트")
+    void 방문_날짜가_유효한_범위를_넘어가면_예외_발생(int invalidDay) {
+        // When, Then
+        Assertions.assertThrows(IllegalArgumentException.class,
+                () -> VisitingDateValidation.isValidDate(String.valueOf(invalidDay)));
     }
 }
