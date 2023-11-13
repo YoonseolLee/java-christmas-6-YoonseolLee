2818e8c57f1bd8dc2e96997aaa44ac788b782a85
Yoonseol Lee <thisyoon97@gmail.com>
2023-11-13 15:56:47 +0900

    feat: 방문일에 받을 이벤트 혜택을 미리 안내(출력)하는 기능

--- src/main/java/christmas/domain/VisitingDate.java
+++ src/main/java/christmas/domain/VisitingDate.java
@@ -1,13 +1,23 @@
 package christmas.domain;
 
 import christmas.domain.validation.VisitingDateValidation;
+import christmas.view.OutputView;
 import java.time.LocalDate;
 
 public class VisitingDate {
+    public LocalDate getVisitingDate() {
+        return visitingDate;
+    }
+
     private LocalDate visitingDate;
 
     public VisitingDate(int day) {
         VisitingDateValidation.validateVisitingDate(day);
         this.visitingDate = LocalDate.of(2023, 12, day);
+    }
+
+    public void printEventAnnae() {
+        OutputView.printMessage("12월 " + visitingDate.getDayOfMonth() + "일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!");
+        OutputView.printNewLine();
     }
 }
