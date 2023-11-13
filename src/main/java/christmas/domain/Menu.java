196501c5afc9ae7689f59a3a75758cd5525aaa77
Yoonseol Lee <thisyoon97@gmail.com>
2023-11-13 10:44:44 +0900

    feat: 메뉴 종류와 메뉴의 수량을 저장하는 Menu 클래스 생성

---
+++ src/main/java/christmas/domain/Menu.java
@@ -0,0 +1,27 @@
+package christmas.domain;
+
+public class Menu {
+    private final MenuBoard menuBoard;
+    private final int quantity;
+
+    public Menu(MenuBoard menuBoard, int quantity) {
+        this.menuBoard = menuBoard;
+        this.quantity = quantity;
+    }
+
+    public MenuBoard getMenuBoard() {
+        return menuBoard;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    @Override
+    public String toString() {
+        return "Menu{" +
+                "menuBoard=" + menuBoard.toString() +
+                ", quantity=" + quantity +
+                '}';
+    }
+}
