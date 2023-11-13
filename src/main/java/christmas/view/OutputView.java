cdc9394370b5e5f7d3485db9776a6a47ae728727
Yoonseol Lee <thisyoon97@gmail.com>
2023-11-14 00:42:49 +0900

    feat: 주말/평일할인 내역을 출력하는 기능

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -1,5 +1,6 @@
 package christmas.view;
 
+import christmas.domain.VisitingDate;
 import christmas.utils.GameMessage;
 
 public class OutputView {
@@ -22,4 +23,20 @@ public class OutputView {
     public static void printNewLine() {
         System.out.println();
     }
+
+
+    public static void printEveryDayDiscountAmount(int everyDayDiscountAmount, VisitingDate visitingDate) {
+        OutputView.printNewLine();
+        if (visitingDate.isWeekend()) {
+            printMessage("주말 할인: -" + everyDayDiscountAmount + "원");
+        }
+        if (!visitingDate.isWeekend()) {
+            printMessage("평일 할인: -" + everyDayDiscountAmount + "원");
+        }
+    }
+//        OutputView.printMessage("크리스마스 디데이 할인: -" + everyDayDiscountAmount + "원");
+//        OutputView.printMessage("특별 할인: ");
+//        OutputView.printMessage("증정 이벤트: ");
 }
+
+
