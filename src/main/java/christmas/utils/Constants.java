db1791dddb7f8f6c6dbf9558f4c5d8c2a9f1006b
Yoonseol Lee <thisyoon97@gmail.com>
2023-11-13 13:30:38 +0900

    refactor: 매직리터럴 정리 및 상수로 등록

--- src/main/java/christmas/utils/Constants.java
+++ src/main/java/christmas/utils/Constants.java
@@ -3,7 +3,5 @@ package christmas.utils;
 public class Constants {
     public static final int START_OF_MONTH = 1;
     public static final int END_OF_MONTH = 31;
-    public static final String KOREAN_MENU_PATTERN = "[가-힣]+";
-    public static final String POSITIVE_INTEGER_PATTERN = "^[1-9][0-9]*$";
     public static final String REGEX_NUMERIC = "\\d+";
 }
