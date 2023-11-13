b6b63b2b0b32b2695292da45e27f96b2256403a9
Yoonseol Lee <thisyoon97@gmail.com>
2023-11-13 19:56:21 +0900

    refactor: VisitingDate 검증과정 리팩토링 정수로 변환 가능한지, 그리고 1과 31사이인지 검증하는 과정을 추가하였음

--- src/main/java/christmas/domain/VisitingDate.java
+++ src/main/java/christmas/domain/VisitingDate.java
@@ -1,6 +1,5 @@
 package christmas.domain;
 
-import christmas.domain.validation.VisitingDateValidation;
 import christmas.view.OutputView;
 import java.time.LocalDate;
 
@@ -12,7 +11,6 @@ public class VisitingDate {
     private LocalDate visitingDate;
 
     public VisitingDate(int day) {
-        VisitingDateValidation.validateVisitingDate(day);
         this.visitingDate = LocalDate.of(2023, 12, day);
     }
 
--- src/main/java/christmas/domain/validation/VisitingDateValidation.java
+++ src/main/java/christmas/domain/validation/VisitingDateValidation.java
@@ -10,7 +10,9 @@ public class VisitingDateValidation {
     private static List<Predicate<String>> validations = Arrays.asList(
             input -> input != null && !input.trim().isEmpty(),
             input -> input.matches(Constants.REGEX_NUMERIC),
-            input -> !input.startsWith("0")
+            input -> !input.startsWith("0"),
+            VisitingDateValidation::isPositiveInteger,
+            input -> isWithinRange(input, 1, 31)
     );
 
     public static boolean isValidDate(String input) {
@@ -21,13 +23,21 @@ public class VisitingDateValidation {
         return isValid;
     }
 
-    public static void validateVisitingDate(int day) {
-        if (!isValidDayRange(day)) {
-            throw new IllegalArgumentException(ErrorMessage.DATE_ERROR_MESSAGE.getMessage());
+    private static boolean isPositiveInteger(String input) {
+        try {
+            int value = Integer.parseInt(input);
+            return value > 0;
+        } catch (NumberFormatException e) {
+            return false;
         }
     }
 
-    public static boolean isValidDayRange(int input) {
-        return input >= Constants.START_OF_MONTH && input <= Constants.END_OF_MONTH;
+    private static boolean isWithinRange(String input, int min, int max) {
+        try {
+            int value = Integer.parseInt(input);
+            return value >= min && value <= max;
+        } catch (NumberFormatException e) {
+            return false;
+        }
     }
 }
