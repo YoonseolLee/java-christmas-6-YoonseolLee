ab3e8e81c85ce15e2686b5bf8b7a6917770e302c
Yoonseol Lee <thisyoon97@gmail.com>
2023-11-13 10:56:13 +0900

    feat: 유효한 방문일 입력을 받을 때까지 반복하는 기능

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -1,15 +1,24 @@
 package christmas.view;
 
 import camp.nextstep.edu.missionutils.Console;
-import christmas.domain.validation.InputValidation;
+import christmas.domain.VisitingDate;
+import christmas.domain.validation.VisitingDateValidation;
 import christmas.utils.GameMessage;
 import christmas.utils.Util;
 
 public class InputView {
-    public static int getVisitingDate() {
-        OutputView.printMessage(GameMessage.ASK_VISITING_DATE.getMessage());
-        String input = Console.readLine();
-        InputValidation.validateVisitingDateInput(input);
-        return Util.convertToInt(input);
+    public static VisitingDate getVisitingDate() {
+        while (true) {
+            try {
+                OutputView.printMessage(GameMessage.ASK_VISITING_DATE.getMessage());
+                String input = Console.readLine();
+                if (VisitingDateValidation.isValidDate(input)) {
+                    int day = Util.convertToInt(input);
+                    return new VisitingDate(day);
+                }
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
     }
 }
