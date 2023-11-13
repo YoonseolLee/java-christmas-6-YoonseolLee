3323baf610b5dd4de3487c8b06b8ae3d50ebeff9
Yoonseol Lee <thisyoon97@gmail.com>
2023-11-14 00:44:47 +0900

    refactor: 유틸리티 메소드를 Util 클래스로 이동

--- src/main/java/christmas/utils/Util.java
+++ src/main/java/christmas/utils/Util.java
@@ -1,7 +1,29 @@
 package christmas.utils;
 
+import christmas.domain.Menu;
+import christmas.domain.MenuBoard;
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
 public class Util {
     public static final int convertToInt(String input) {
         return Integer.parseInt(input);
+    }
+
+    public static final List<String> convertInputToMenuNames(String input) {
+        return Arrays.asList(input.split(","));
+    }
+
+    public static final List<Menu> getOrderedMenus(List<String> menuNames) {
+        Pattern pattern = Pattern.compile("(.+)-([0-9]+)");
+
+        return menuNames.stream()
+                .map(pattern::matcher)
+                .filter(Matcher::matches)
+                .map(matcher -> new Menu(MenuBoard.valueOf(matcher.group(1)), Integer.parseInt(matcher.group(2))))
+                .collect(Collectors.toList());
     }
 }
--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -2,7 +2,6 @@ package christmas.view;
 
 import camp.nextstep.edu.missionutils.Console;
 import christmas.domain.Menu;
-import christmas.domain.MenuBoard;
 import christmas.domain.Order;
 import christmas.domain.VisitingDate;
 import christmas.domain.validation.OrderValidation;
@@ -10,19 +9,16 @@ import christmas.domain.validation.VisitingDateValidation;
 import christmas.utils.ErrorMessage;
 import christmas.utils.GameMessage;
 import christmas.utils.Util;
-import java.util.Arrays;
 import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-import java.util.stream.Collectors;
 
 public class InputView {
     public static VisitingDate getVisitingDate() {
+        OutputView.printMessage(GameMessage.GREETINGS_MESSAGE.getMessage());
         while (true) {
             try {
                 String input = getUserInput(GameMessage.ASK_VISITING_DATE.getMessage());
                 validateDate(input);
-                int day = convertToInt(input);
+                int day = Util.convertToInt(input);
                 return new VisitingDate(day);
             } catch (IllegalArgumentException e) {
                 OutputView.printMessage(e.getMessage());
@@ -41,15 +37,11 @@ public class InputView {
         }
     }
 
-    private static int convertToInt(String input) {
-        return Util.convertToInt(input);
-    }
-
     public static Order getOrder() {
         while (true) {
             try {
                 List<String> menuNames = getMenuNames();
-                List<Menu> orderedMenus = getOrderedMenus(menuNames);
+                List<Menu> orderedMenus = Util.getOrderedMenus(menuNames);
                 return new Order(menuNames, orderedMenus);
             } catch (IllegalArgumentException e) {
                 OutputView.printMessage(e.getMessage());
@@ -62,7 +54,7 @@ public class InputView {
         do {
             orderInput = receiveOrderInput();
         } while (!OrderValidation.isValidOrder(orderInput));
-        return convertInputToMenuNames(orderInput);
+        return Util.convertInputToMenuNames(orderInput);
     }
 
     private static String receiveOrderInput() {
@@ -73,19 +65,5 @@ public class InputView {
             OutputView.printMessage(e.getMessage());
             return "";
         }
-    }
-
-    private static List<String> convertInputToMenuNames(String input) {
-        return Arrays.asList(input.split(","));
-    }
-
-    public static List<Menu> getOrderedMenus(List<String> menuNames) {
-        Pattern pattern = Pattern.compile("(.+)-([0-9]+)");
-
-        return menuNames.stream()
-                .map(pattern::matcher)
-                .filter(Matcher::matches)
-                .map(matcher -> new Menu(MenuBoard.valueOf(matcher.group(1)), Integer.parseInt(matcher.group(2))))
-                .collect(Collectors.toList());
     }
 }
