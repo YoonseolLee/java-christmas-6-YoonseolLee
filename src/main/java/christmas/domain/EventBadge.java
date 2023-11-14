5650c0eaee1adb9c92a8ac39d1aeeda4411c2e7a
Yoonseol Lee <thisyoon97@gmail.com>
2023-11-14 13:56:56 +0900

    feat: EventBadge enum 생성

---
+++ src/main/java/christmas/domain/EventBadge.java
@@ -0,0 +1,36 @@
+package christmas.domain;
+
+public enum EventBadge {
+    STAR(5000, "별"),
+    TREE(10000, "트리"),
+    SANTA(20000, "산타");
+
+    private final int threshold;
+    private final String badge;
+
+    EventBadge(int threshold, String badge) {
+        this.threshold = threshold;
+        this.badge = badge;
+    }
+
+    public int getThreshold() {
+        return threshold;
+    }
+
+    public String getBadge() {
+        return badge;
+    }
+
+    public static String getBadgeByTotalBenefitAmount(int totalBenefitAmount) {
+        if (totalBenefitAmount >= SANTA.getThreshold()) {
+            return SANTA.getBadge();
+        }
+        if (totalBenefitAmount >= TREE.getThreshold()) {
+            return TREE.getBadge();
+        }
+        if (totalBenefitAmount >= STAR.getThreshold()) {
+            return STAR.getBadge();
+        }
+        return "없음";
+    }
+}
