71790e64ce4acb908e36b5208290e19e0c109091
Yoonseol Lee <thisyoon97@gmail.com>
2023-11-14 12:55:57 +0900

    feat: 할인을 계산하는 기능

--- src/main/java/christmas/controller/MainController.java
+++ src/main/java/christmas/controller/MainController.java
@@ -5,28 +5,47 @@ import christmas.domain.Order;
 import christmas.domain.VisitingDate;
 import christmas.view.InputView;
 import christmas.view.OutputView;
+import java.text.DecimalFormat;
 
 public class MainController {
+    DiscountCalculator calculator;
+
     public void start() {
         VisitingDate visitingDate = receiveVisitingDate();
         Order order = receiveOrder();
         printOrderedMenus(visitingDate, order);
         printTotalPriceBeforeDiscount(order);
+        DiscountCalculator.calculateGiveAwayEvent(order);
+        DiscountCalculator.printGiveaway();
 
-        OutputView.printGiveaways();
         // 크리스마스 디데이 할인
         OutputView.printNewLine();
         OutputView.printMessage("<혜택 내역>");
         int dDayDiscountAmount = DiscountCalculator.calculateDDayDiscount(visitingDate);
         OutputView.printDdayDiscountAmount(dDayDiscountAmount);
+
         // 평일/주말할인
         int everyDayDiscountAmount = DiscountCalculator.calculateEveryDayDiscount(order, visitingDate);
         OutputView.printEveryDayDiscountAmount(everyDayDiscountAmount, visitingDate);
+
         // 특별할인
         int specialDiscountAmount = DiscountCalculator.calculateSpeicalDiscount(visitingDate);
         OutputView.printSpecialDiscountAmount(specialDiscountAmount);
-        // 증정이벤트
 
+        // 증정이벤트
+        DiscountCalculator.printGiveawayBenefit();
+
+        // 총혜택금액
+        DiscountCalculator.calculateTotalBenefitAmount();
+        int totalBenefitAmount = DiscountCalculator.getTotalBenefitAmount();
+        DecimalFormat formatter = new DecimalFormat("#,###");
+        System.out.println("<총혜택 금액>");
+        System.out.println(formatter.format(totalBenefitAmount) + "원");
+        System.out.println();
+
+        // 할인 후 예상 결제 금액
+        int totalPriceAfterDiscount = order.getTotalPriceAfterDiscount(totalBenefitAmount);
+        order.printTotalPriceAfterDiscount(totalPriceAfterDiscount);
     }
 
     private VisitingDate receiveVisitingDate() {
@@ -47,6 +66,4 @@ public class MainController {
     private void printTotalPriceBeforeDiscount(Order order) {
         order.printTotalPriceBeforeDiscount();
     }
-
-
 }
--- src/main/java/christmas/domain/DiscountCalculator.java
+++ src/main/java/christmas/domain/DiscountCalculator.java
@@ -1,34 +1,68 @@
 package christmas.domain;
 
-public class DiscountCalculator {
-    private static final int WEEKEND_DISCOUNT_AMOUNT = 2023;
-    private static final int WEEKDAY_DISCOUNT_AMOUNT = 2023;
-    public static int totalDiscount = 0;
+import christmas.view.OutputView;
 
-    public static int getTotalDiscount() {
-        return totalDiscount;
+public class DiscountCalculator {
+    private static final int WEEKEND_DISCOUNT_AMOUNT = -2023;
+    private static final int WEEKDAY_DISCOUNT_AMOUNT = -2023;
+    public static int totalDiscount = 0;
+    public static int giveawayBenefit = 0;
+    public static int totalBenefitAmount = 0;
+    public static int champagneForFree = 0;
+    private static boolean isGiveawayPriceCalculated = false;
+
+    public static void calculateGiveAwayEvent(Order order) {
+        if (order.isTotalPriceAboveThreshold()) {
+            giveawayBenefit -= 25000;
+            champagneForFree++;
+        }
     }
 
+    public static void printGiveaway() {
+        OutputView.printGiveaways(champagneForFree);
+    }
 
-    public static int calculateEveryDayDiscount(Order order, VisitingDate visitingDate) {
-        int everyDayDiscountAmount = order.getMainCount() * WEEKEND_DISCOUNT_AMOUNT;
-        if (!visitingDate.isWeekend()) {
-            everyDayDiscountAmount = order.getDessertCount() * WEEKDAY_DISCOUNT_AMOUNT;
-        }
-        totalDiscount = totalDiscount + everyDayDiscountAmount;
-        return everyDayDiscountAmount;
+    public static void printGiveawayBenefit() {
+        OutputView.printGiveawayBenefit(giveawayBenefit);
     }
 
     public static int calculateDDayDiscount(VisitingDate visitingDate) {
         int dDayDiscountAmount = visitingDate.calculateDDayDiscountAmount();
-        totalDiscount = totalDiscount + dDayDiscountAmount;
+        totalDiscount += dDayDiscountAmount;
         return dDayDiscountAmount;
     }
 
+    public static int calculateEveryDayDiscount(Order order, VisitingDate visitingDate) {
+        int everyDayDiscountAmount;
+        if (visitingDate.isWeekend()) {
+            everyDayDiscountAmount = order.getMainCount() * WEEKEND_DISCOUNT_AMOUNT;
+        } else {
+            everyDayDiscountAmount = order.getDessertCount() * WEEKDAY_DISCOUNT_AMOUNT;
+        }
+        totalDiscount += everyDayDiscountAmount;
+        return everyDayDiscountAmount;
+    }
+
+
     public static int calculateSpeicalDiscount(VisitingDate visitingDate) {
         int specialDiscountAmount = visitingDate.calculateSpecialDiscountAmount();
-        totalDiscount = totalDiscount + specialDiscountAmount;
+        totalDiscount += specialDiscountAmount; // 수정된 부분
         return specialDiscountAmount;
+    }
+
+
+    public static void calculateTotalBenefitAmount() {
+        totalBenefitAmount = totalDiscount + giveawayBenefit;
+    }
+
+    public static int getTotalBenefitAmount() {
+        return totalBenefitAmount;
+    }
+
+    public static int calculateDdayDiscountAmount(VisitingDate visitingDate) {
+        int dDayDiscountAmount = visitingDate.calculateDDayDiscountAmount();
+        totalDiscount = totalDiscount + dDayDiscountAmount;
+        return dDayDiscountAmount;
     }
 }
 
