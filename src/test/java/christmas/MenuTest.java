3c8f62d5c5194a67b5855c4beac5b8da0de4e3d1
Yoonseol Lee <thisyoon97@gmail.com>
2023-11-15 14:07:21 +0900

    refactor: Menu 테스트코드 재작성

--- src/test/java/christmas/MenuTest.java
+++ src/test/java/christmas/MenuTest.java
@@ -1,40 +1,47 @@
 package christmas;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 import christmas.domain.Menu;
 import christmas.domain.MenuBoard;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class MenuTest {
-    @DisplayName("GetMenuBoard() 유효성 검사")
-    @Test
-    public void testGetMenuBoard() {
-        // Given
-        MenuBoard expectedMenuBoard = MenuBoard.양송이수프;
-        int quantity = 2;
+    private Menu menuMain;
+    private Menu menuDessert;
 
-        // When
-        Menu menu = new Menu(expectedMenuBoard, quantity);
-        MenuBoard actualMenuBoard = menu.getMenuBoard();
-
-        // Then
-        assertEquals(expectedMenuBoard, actualMenuBoard);
+    @BeforeEach
+    public void setUp() {
+        menuMain = new Menu(MenuBoard.티본스테이크, 2);
+        menuDessert = new Menu(MenuBoard.초코케이크, 1);
     }
 
-    @DisplayName("GetQuantity() 유효성 검사")
+    @DisplayName("GetMenuBoard 테스트")
     @Test
-    public void testGetQuantity() {
-        // Given
-        MenuBoard menuBoard = MenuBoard.타파스;
-        int expectedQuantity = 3;
+    public void getMenuBoard를_실행하면_name을_불러온다() {
+        Assertions.assertThat(menuMain.getMenuBoard()).isEqualTo(MenuBoard.티본스테이크);
+        Assertions.assertThat(menuDessert.getMenuBoard()).isEqualTo(MenuBoard.초코케이크);
+    }
 
-        // When
-        Menu menu = new Menu(menuBoard, expectedQuantity);
-        int actualQuantity = menu.getQuantity();
+    @DisplayName("GetQuantity 테스트")
+    @Test
+    public void getQuantity를_실행하면_quantity를_불러온다() {
+        Assertions.assertThat(menuMain.getQuantity()).isEqualTo(2);
+        Assertions.assertThat(menuDessert.getQuantity()).isEqualTo(1);
+    }
 
-        // Then
-        assertEquals(expectedQuantity, actualQuantity);
+    @DisplayName("isDessert 테스트")
+    @Test
+    public void 디저트_메뉴이면_true를_반환한다() {
+        Assertions.assertThat(menuMain.isDessert()).isFalse();
+        Assertions.assertThat(menuDessert.isDessert()).isTrue();
+    }
+
+    @DisplayName("isMain 테스트")
+    @Test
+    public void 메인_메뉴이면_true를_반환한다() {
+        Assertions.assertThat(menuMain.isMain()).isTrue();
+        Assertions.assertThat(menuDessert.isMain()).isFalse();
     }
 }
