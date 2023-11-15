161e812f2f6df566c96742fc1de9b4035d244550
Yoonseol Lee <thisyoon97@gmail.com>
2023-11-15 19:21:56 +0900

    refactor: EventBadge 테스트코드 작성

--- src/main/java/christmas/utils/Constants.java
+++ src/main/java/christmas/utils/Constants.java
@@ -3,11 +3,11 @@ package christmas.utils;
 public class Constants {
     public static final String NUMBER_FORMAT_PATTERN = "#,###";
     public static final String EMPTY_STRING = "";
+    public static final String REGEX_NUMERIC = "\\d+";
     public static final int START_OF_MONTH = 1;
     public static final int END_OF_MONTH = 31;
     public static final int FIRST_DAY_FOR_DDAY_EVENT = 1;
     public static final int LAST_DAY_FOR_DDAY_EVENT = 31;
-    public static final String REGEX_NUMERIC = "\\d+";
     public static final int MAXIMUM_ORDER_QUANTITY = 20;
     public static final int WEEKEND_DISCOUNT_AMOUNT = -2023;
     public static final int WEEKDAY_DISCOUNT_AMOUNT = -2023;
