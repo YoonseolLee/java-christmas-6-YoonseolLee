4e885be0fb6b8458ed32a3879a6cd12eb77afa6b
Yoonseol Lee <thisyoon97@gmail.com>
2023-11-15 17:04:24 +0900

    refactor: Order 내 메소드 재정렬

--- src/main/java/christmas/domain/order/Order.java
+++ src/main/java/christmas/domain/order/Order.java
@@ -9,34 +9,26 @@ public class Order {
     private final List<Menu> orderedMenus;
     private final int totalPriceBeforeDiscount;
 
-//    [해산물파스타-2, 레드와인-1, 초코케이크-1]
-//    [Menu{menuBoard=해산물파스타, quantity=2}, Menu{menuBoard=레드와인, quantity=1}, Menu{menuBoard=초코케이크, quantity=1}]
-
     public Order(List<String> menuNames, List<Menu> orderedMenus) {
         this.menuNames = menuNames;
         this.orderedMenus = orderedMenus;
         this.totalPriceBeforeDiscount = calculateTotalPriceBeforeDiscount();
     }
 
-    public boolean hasValidTotalPriceForEvents() {
-        return totalPriceBeforeDiscount >= Constants.MINIMUM_PRICE_FOR_ALL_EVENTS;
-    }
-
-    private int calculateTotalPriceBeforeDiscount() {
-        return orderedMenus.stream()
-                .mapToInt(menu -> menu.getMenuBoard().getPrice() * menu.getQuantity())
-                .sum();
-    }
-
     public List<Menu> getOrderedMenus() {
         return orderedMenus;
     }
 
-    private int getCountByMenuType(Predicate<Menu> predicate) {
-        return orderedMenus.stream()
-                .filter(predicate)
-                .mapToInt(Menu::getQuantity)
-                .sum();
+    public int getTotalPriceBeforeDiscount() {
+        return totalPriceBeforeDiscount;
+    }
+
+    public boolean hasValidTotalPriceForEvents() {
+        return totalPriceBeforeDiscount >= Constants.MINIMUM_PRICE_FOR_ALL_EVENTS;
+    }
+
+    public boolean isTotalPriceAboveThreshold() {
+        return totalPriceBeforeDiscount >= Constants.PRICE_THRESHOLD_FOR_GIVEAWAY_EVENT;
     }
 
     public int getDessertCount() {
@@ -47,26 +39,22 @@ public class Order {
         return getCountByMenuType(Menu::isMain);
     }
 
-    public boolean isTotalPriceAboveThreshold() {
-        return totalPriceBeforeDiscount >= Constants.PRICE_THRESHOLD_FOR_GIVEAWAY_EVENT;
-    }
-
     public int getTotalPriceAfterDiscount(int totalBenefitAmount) {
-        int totalPriceAfterDiscount = 0;
-        if (isTotalPriceAboveThreshold()) {
-            totalPriceAfterDiscount = totalPriceBeforeDiscount - (-totalBenefitAmount) + (25_000);
-        }
-        if (!isTotalPriceAboveThreshold()) {
-            totalPriceAfterDiscount = totalPriceBeforeDiscount - (-totalBenefitAmount);
-        }
-        return totalPriceAfterDiscount;
+        return isTotalPriceAboveThreshold()
+                ? totalPriceBeforeDiscount - (-totalBenefitAmount) + (25_000)
+                : totalPriceBeforeDiscount - (-totalBenefitAmount);
     }
 
-    public List<String> getMenuNames() {
-        return menuNames;
+    private int calculateTotalPriceBeforeDiscount() {
+        return orderedMenus.stream()
+                .mapToInt(menu -> menu.getMenuBoard().getPrice() * menu.getQuantity())
+                .sum();
     }
 
-    public int getTotalPriceBeforeDiscount() {
-        return totalPriceBeforeDiscount;
+    private int getCountByMenuType(Predicate<Menu> predicate) {
+        return orderedMenus.stream()
+                .filter(predicate)
+                .mapToInt(Menu::getQuantity)
+                .sum();
     }
 }
